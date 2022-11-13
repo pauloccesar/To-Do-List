bb8636c2a3cfc5bb8eb8ed50861e1682020587a3
pauloccesar <pauloccesar.131@gmail.com>
2022-11-13 12:32:28 -0300

    create function delete

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
 
+  public taskList: Array<TaskList> = [
+    { task: 'Minha nova task', checked: false },
+    { task: 'Minha nova task', checked: false },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public deleteItemTaskList(event: number) {
+    this.taskList.splice(event, 1);
+  }
+
+  public deleteAllTaskList() {
+    const confirm = window.confirm("Você deseja realmente Deletar tudo?");
+
+    if (confirm) {
+      this.taskList = [];
+    }
+  }
 }
